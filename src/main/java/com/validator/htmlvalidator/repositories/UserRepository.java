c1375f2ac5522a8336ae75eeb44752ecfbeb24f1
Kispál Zoltán <kispalzoltan99@gmail.com>
2024-05-11 00:36:20 +0200

    server build fix

--- src/main/java/com/validator/htmlvalidator/repositories/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.validator.htmlvalidator.repositories;
-
-import com.validator.htmlvalidator.models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {}
---
+++ src/main/java/com/validator/htmlvalidator/services/OpenaiService.java
@@ -0,0 +1,2 @@
+package com.validator.htmlvalidator.services;public class OpenaiService {
+}
